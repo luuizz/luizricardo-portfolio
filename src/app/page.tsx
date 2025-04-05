6d91c5125526d43496fe7465028c682ff87a20a0
Luiz Ricardo <luu.izz@hotmail.com>
2025-04-05 15:40:16 -0300

    feat: adicionado componentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+import CallToAction from "@/components/sections/cta";
+import SectionDiferenciais from "@/components/sections/diferenciais";
+import SectionHero from "@/components/sections/hero";
+import SectionMarquee from "@/components/sections/marquee";
+import SectionProjetos from "@/components/sections/projetos";
+import SectionSobre from "@/components/sections/sobre";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-semibold underline">
-        Hello world!
-      </h1>
-    </main>
+    <>
+      <SectionHero />
+      <SectionSobre />
+      <SectionMarquee />
+      <SectionProjetos />
+      <SectionDiferenciais />
+      <CallToAction />
+    </>
   );
 }
