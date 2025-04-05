f66e47fdb1abcd5269ae60ab35cb3ccac038271b
Luiz Ricardo <luu.izz@hotmail.com>
2025-04-05 14:08:45 -0300

    começo do projeto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({
@@ -7,9 +7,15 @@ const inter = Inter({
   subsets: ["latin"],
 });
 
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: "600"
+});
+
 export const metadata: Metadata = {
-  title: "Boilerplate Luric.web",
-  description: "Criado e gerenciado por Luric.web",
+  title: "Luiz Ricardo | Desenvolvedor Front-end",
+  description: "Olá, sou o Luiz Ricardo, um desenvolvedor front-end apaixonado por criar experiências digitais incriveis. Neste portfólio, você poderá explorar meus trabalhos criativos, recomendações e habilidades adquiridas ao longo da minha jornada profissional. Sinta-se à vontade para se conectar comigo no LinkedIn e explorar meus projetos no Github para ter uma visão mais aprofundada do meu expertise técnico e criativo.",
 };
 
 export default function RootLayout({
@@ -20,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body
-        className={`${inter.variable} antialiased`}
+        className={`${inter.variable} ${poppins.variable} antialiased`}
       >
         {children}
       </body>
