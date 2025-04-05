f41f6a165c48e845cbe6376af6d9da376addb7a4
Luiz Ricardo <luu.izz@hotmail.com>
2025-04-05 15:40:07 -0300

    feat: construído o header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,48 @@
 import React from 'react'
+import Grid from '../grid'
+import Link from 'next/link'
+import Image from 'next/image'
+import { menuLinks, socialLinks } from '@/app/shared/utils/global-data'
 
 export default function Header() {
   return (
-    <header>Header</header>
+    <header className='sticky top-0 z-50 py-8 bg-black/80 backdrop-blur-2xl'>
+      <Grid className='flex items-center justify-between'>
+        <Link href="/" title='Ir para a página inicial'>
+          <Image src="logo.svg"
+          alt='Logo do Luiz Ricardo'
+          width={160}
+          height={41}
+          />
+        </Link>
+
+        <nav className='flex items-center justify-between gap-4'>
+          <ul className='flex items-center gap-6'>
+            {
+              menuLinks.map((item, index) => (
+                <li key={index}>
+                  <Link className='font-normal text-white transition-colors duration-300 text-base/short hover:text-brand-primary-default' href={item.url} title={item.title}>
+                    {item.title}
+                  </Link>
+                </li>
+              ))
+            }
+          </ul>
+          <div className='w-0.5 h-6 bg-brand-gray-700' />
+
+          <ul className='flex items-center gap-3'>
+            {
+              socialLinks.map((item, index) => (
+                <li key={index}>
+                  <Link className='group' href={item.url} title={item.title}>
+                    <item.icon className='text-white hover:group-hover:text-brand-primary-default' size={32} />
+                  </Link>
+                </li>
+              ))
+            }
+          </ul>
+        </nav>
+      </Grid>
+    </header>
   )
 }
