86205647ec545398d117db217bd20058b915ddc9
Luiz Ricardo <luu.izz@hotmail.com>
2025-04-05 15:39:58 -0300

    feat: estendido o tema

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,12 +9,33 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'brand-primary': {
+          'light': '#FFF4BE',
+          'default': '#FFD300',
+          'dark': '#816B05',
+        },
+        'brand-gray': {
+          100: "#F6F6F6",
+          200: "#DDDDDA",
+          300: "#C8C7C6",
+          400: "#BDBDBA",
+          500: "#9E9E9A",
+          600: "#7A7A78",
+          700: "#52514F",
+          800: "#35342D",
+          900: "#151203",
+        }
       },
       fontFamily: {
         inter: ["var(--font-inter)", "sans-serif"],
         poppins: ["var(--font-poppins)", "sans-serif"],
+      },
+      maxWidth: {
+        container: "77.875rem",
+      },
+      lineHeight: {
+        'short': '120%',
+        'large': '150%',
       }
     },
   },
