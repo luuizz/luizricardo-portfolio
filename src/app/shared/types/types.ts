300a048dbc36ff27d550dd81f7c9f6c72430d9d1
Luiz Ricardo <luu.izz@hotmail.com>
2025-04-05 15:24:59 -0300

    feat: criado tipagem

---
+++ src/app/shared/types/types.ts
@@ -0,0 +1,14 @@
+import { IconType } from "react-icons";
+
+export type MenuLink = {
+  id: number;
+  title: string;
+  url: string;
+}
+
+export type SocialLink = {
+  id: number;
+  title: string;
+  url: string;
+  icon: IconType
+}
