a5de054e60050dc31090b7aaafaf727999358495
Luiz Ricardo <luu.izz@hotmail.com>
2025-04-09 22:37:31 -0300

    feat: criado hook para encapsular animações

---
+++ src/hooks/useScrollReveal.ts
@@ -0,0 +1,64 @@
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+import gsap from "gsap";
+import { RefObject } from "react";
+
+type Direction = "left" | "right" | "top" | "bottom";
+
+interface ScrollRevealOptions {
+  direction?: Direction,
+  duration?: number;
+  delay?: number;
+  stagger?: number;
+}
+
+export function useScrollReveal(
+  ref: RefObject<HTMLElement | null>, 
+  options: ScrollRevealOptions = {}
+){
+  const { direction = 'left', duration = 1, delay, stagger } = options
+
+  useGSAP(() => {
+    if (!ref.current) return;
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    const getFromValues = () => {
+      switch (direction) {
+        case 'left':
+          return { x: -50, y: 0 }
+        case 'right':
+          return { x: 50, y: 0 }
+        case 'top':
+          return { x: 0, y: -50 }
+        case 'bottom':
+          return { x: 0, y: 50 }
+        default:
+          return { x: 0, y: 0 }
+      }
+    }
+
+    const from = {
+      opacity: 0,
+      ...getFromValues(),
+    }
+
+    const to = {
+      opacity: 1,
+      x: 0,
+      y: 0,
+      ease: 'power3.out',
+      duration,
+      delay,
+      stagger,
+      scrollTrigger: {
+        trigger: ref.current,
+        start: 'top 80%',
+        toggleActions: 'play reverse play reverse',
+      },
+    }
+
+    gsap.fromTo(ref.current, from, to);
+
+  }, [ref])
+}
