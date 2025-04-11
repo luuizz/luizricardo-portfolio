2a9f6fd75eb8c9f690b4bde3475740fc91061b31
Luiz Ricardo <luu.izz@hotmail.com>
2025-04-10 22:04:32 -0300

    feat: adicionado SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,20 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: "Luiz Ricardo | Desenvolvedor Front-end",
   description: "Olá, sou o Luiz Ricardo, um desenvolvedor front-end apaixonado por criar experiências digitais incriveis. Neste portfólio, você poderá explorar meus trabalhos criativos, recomendações e habilidades adquiridas ao longo da minha jornada profissional. Sinta-se à vontade para se conectar comigo no LinkedIn e explorar meus projetos no Github para ter uma visão mais aprofundada do meu expertise técnico e criativo.",
+  keywords: "desenvolvedor front-end, react, angular, profissional programador, programação front-end, recrutador front-end, typescript, nodejs",
+  authors: [{ name: "Luiz Ricardo", url: "https://github.com/luuizz" }],
+  publisher: "Luiz Ricardo",
+  openGraph: {
+    title: "Luiz Ricardo | Desenvolvedor Front-end",
+    images: [
+      {
+        url: "/share-og.png",
+        width: 1200,
+        height: 628,
+        alt: "Luiz Ricardo | Desenvolvedor Front-end",
+      },
+    ]
+  }
 };
 
 export default function RootLayout({
