ec9d6c868156b9c04a2008f517e14931702cc2bc
Luiz Ricardo <luu.izz@hotmail.com>
2025-04-10 21:41:38 -0300

    feat:finalizado seção de diferenciais

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,7 +49,7 @@ export default function Home() {
       <SectionSobre />
       <SectionMarquee />
       <SectionProjetos />
-      {/* <SectionDiferenciais /> */}
+      <SectionDiferenciais />
       <CallToAction />
     </>
   );
--- src/components/sections/diferenciais/index.tsx
+++ src/components/sections/diferenciais/index.tsx
@@ -14,15 +14,15 @@ export default function SectionDiferenciais() {
   useScrollReveal(paragraphRef, { direction: 'right', duration: 1, delay: 0.5 });
 
   return (
-    <section  id='diferenciais' className='py-28 scroll-mt-header'>
+    <section  id='diferenciais' className='overflow-hidden py-20 md:py-28 scroll-mt-header'>
       <Grid>
-        <div className='flex items-center justify-between mb-16'>
-          <h2 ref={titleRef} className='text-4xl/short text-brand-gray-900'>Meus diferenciais</h2>
+        <div className='flex flex-col md:flex-row gap-8 text-center md:text-left items-center justify-between mb-16'>
+          <h2 ref={titleRef} className='text-5xl md:text-4xl/short text-brand-gray-900'>Meus diferenciais</h2>
 
           <p ref={paragraphRef} className='w-96  text-base/large text-brand-gray-600'><strong className='text-brand-gray-900 font-inter'>Transformo design</strong> em páginas para a web com agilidade, qualidade e <strong className='text-brand-gray-900 font-inter'>alta perfomance.</strong></p>
         </div>
 
-        <div className='grid grid-cols-3 gap-8'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 md:gap-8'>
           {
             dadosDiferencial.map((item) => (
               <CardDiferencial key={item.id} {...item} />
