7fcc6a997af3287edfadf4a2dd9f02532c03e2fe
Luiz Ricardo <luu.izz@hotmail.com>
2025-10-07 20:57:03 -0300

    feat: adicionado novos items para o sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,15 +1,34 @@
-import { MetadataRoute } from "next"
+import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-
-  const baseUrl = 'https://luizricardotech.com.br'
+  const baseUrl = "https://luizricardotech.com.br";
 
   return [
     {
       url: `${baseUrl}/`,
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: "yearly",
       priority: 1,
     },
-  ]
+    {
+      url: `${baseUrl}/contato`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 3,
+    },
+
+    {
+      url: `${baseUrl}/projeto`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 4,
+    },
+
+    {
+      url: `${baseUrl}/blog`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 5,
+    },
+  ];
 }
