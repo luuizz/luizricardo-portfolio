84ec2b81303481b587e2068d28bbfbde773be39e
Luiz Ricardo <luu.izz@hotmail.com>
2025-10-09 22:53:38 -0300

    feat: tipado o client

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,6 +1,9 @@
 import { createBrowserClient } from "@supabase/ssr";
+import type { SupabaseClient } from "@supabase/supabase-js";
+import type { Database } from "@/types/supabase";
 
-export const supabaseBrowser = createBrowserClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-);
+export const supabaseBrowser: SupabaseClient<Database> =
+  createBrowserClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  );
